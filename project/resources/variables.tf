f38edaa16f6511006d5471246b4e30868fb9045e
kyoheitsuno <freetrip555@gmail.com>
2022-05-09 18:11:27 +0900

    add variables.tf

---
+++ project/resources/variables.tf
@@ -0,0 +1,19 @@
+variable "project_id" {
+  default = ""
+}
+
+variable "env" {
+  default = ""
+}
+
+variable "env_location" {
+  default = ""
+}
+
+variable "region" {
+  default = ""
+}
+
+variable "env_production" {
+  default = false
+}
